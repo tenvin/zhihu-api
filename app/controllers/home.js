af55442ca2d2affef643c70c9a6b5ff24f18225b
tenvin <tenvin@gmail.com>
2020-05-15 15:24:30 +0800

    1

---
+++ app/controllers/home.js
@@ -0,0 +1,8 @@
+const path = require('path')
+class HomeCtl {
+    index(ctx) {
+        ctx.body = '<h1>这是主页</h1>'
+    }
+}
+
+module.exports = new HomeCtl()
--- app/index.js
+++ app/index.js
@@ -2,14 +2,14 @@ const Koa = require('koa')
 const bodyparser = require('koa-body')
 const mongoose = require('mongoose')
 const app = new Koa()
-//const routing = require('./routes')
+const routing = require('./routes')
 
 const mongoURL ="mongodb://localhost:27017/zhihu"
 mongoose.connect(mongoURL, { useNewUrlParser: true,useUnifiedTopology: true }, () => console.log('MongoDB 连接成功了！'))
 mongoose.connection.on('error', console.error);
 
 
-//app.use(bodyparser())
-//routing(app)
+app.use(bodyparser())
+routing(app)
 
 app.listen(3000, () => console.log('程序启动在3000端口了'))
