b5c1b0f41685f789c45b56255e4bb4bec31b9e26
tenvin <tenvin@gmail.com>
2020-05-15 09:11:19 +0800

    1

--- app/index.js
+++ app/index.js
@@ -1,9 +1,15 @@
 const Koa = require('koa')
-const bodyparser = require('koa-bodyparser')
+const bodyparser = require('koa-body')
+const mongoose = require('mongoose')
 const app = new Koa()
-const routing = require('./routes')
+//const routing = require('./routes')
 
-app.use(bodyparser())
-routing(app)
+const mongoURL ="mongodb://localhost:27017/zhihu"
+mongoose.connect(mongoURL, { useNewUrlParser: true,useUnifiedTopology: true }, () => console.log('MongoDB 连接成功了！'))
+mongoose.connection.on('error', console.error);
+
+
+//app.use(bodyparser())
+//routing(app)
 
 app.listen(3000, () => console.log('程序启动在3000端口了'))
